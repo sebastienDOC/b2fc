9570fae0eb2d16904cb62799cdf71bcea30509dc
sebastienDOC <s.dechand21@gmail.com>
2023-07-28 15:05:13 +0200

    filtres

--- src/components/Gallerie/Gallery.jsx
+++ src/components/Gallerie/Gallery.jsx
@@ -1,26 +1,33 @@
+import { useState } from 'react'
 import gallery from '../../datas/images.json'
 import Card from './Card'
 
 function Gallery() {
+    const [items, setItems] = useState(gallery)
+    function filterItem(categorieItem) {
+        const updatedItems = gallery.filter((currentElement) => {
+            return currentElement.categorie === categorieItem
+        })
+        setItems(updatedItems)
+    }
     return (
         <div className='center' id='gallerie'>
             <h1>Portfolio</h1>
             <div className='filtres'>
-                <p>Tous</p>
-                <p>Entrée</p>
-                <p>Salon</p>
-                <p>Cuisine</p>
-                <p>Chambre</p>
-                <p>Salle de bain</p>
-                <p>Toilettes</p>
-                <p>Escaliers</p>
-                <p>Terrasse</p>
-                <p>Piscine</p>
+                <button onClick={() => setItems(gallery)}>Tous</button>
+                <button onClick={() => filterItem('Salon')}>Salon</button>
+                <button onClick={() => filterItem('Cuisine')}>Cuisine</button>
+                <button onClick={() => filterItem('Chambre')}>Chambre</button>
+                <button onClick={() => filterItem('Salle de bain')}>Salle de bain</button>
+                <button onClick={() => filterItem('Toilettes')}>Toilettes</button>
+                <button onClick={() => filterItem('Escaliers')}>Escaliers</button>
+                <button onClick={() => filterItem('Terrasse')}>Terrasse</button>
+                <button onClick={() => filterItem('Piscine')}>Piscine</button>
             </div>
             <div className='grid'>
-                {gallery.map((data) => 
+                {items.map((data) => 
                     <Card 
-                        id={data.id}
+                        key={data.id}
                         image={data.image}
                         titre={data.titre}
                     />
