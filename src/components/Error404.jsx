4f6bc136a46e3b0949f376b418cd62beea06612d
sebastienDOC <s.dechand21@gmail.com>
2023-07-28 17:23:41 +0200

    gh pages

---
+++ src/components/Error404.jsx
@@ -0,0 +1,20 @@
+import Logo from '../assets/logo.jpg'
+import Insta from '../assets/insta.png'
+import { Link } from 'react-router-dom'
+
+function Error() {
+    return (
+        <div className='nav_flex'> 
+            <img className="nav_logo" src={Logo} alt={Logo} />
+            <nav>
+                <Link to="/">Accueil</Link>
+                <Link to="/gallery">Gallerie</Link>
+                <a href="https://www.instagram.com/steph_maash/" target='_blank' rel="noopener noreferrer">
+                    <img src={Insta} alt={Insta} />
+                </a>
+            </nav>
+        </div>
+    )
+}
+
+export default Error
