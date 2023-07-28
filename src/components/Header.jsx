8c42cf62244c4f94c8def54cb9f85d1e496bf4b8
sebastienDOC <s.dechand21@gmail.com>
2023-07-28 14:25:24 +0200

    ghpages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,10 @@ function Header() {
     return (
         <div className='nav_flex'> 
             <img className="" src={Logo} alt={Logo} />
+            <span className='title'>
+                <h1>B2F</h1>
+                <h2>Carrelage</h2>
+            </span>
             <nav>
                 <a href="#about">A propos</a>
                 <a href="#gallerie">Gallerie</a>
